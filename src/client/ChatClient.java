5e68e5da03aadba1179332a6fb6e117f2f001225
GG <GG@DESKTOP-B2UISU1>
2021-04-08 16:27:31 +0900

    대화 및 귓말 제작

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -1,7 +1,51 @@
 package client;
 
-public class ChatClient {
-	public static void main(String[] args) {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
+public class ChatClient {
+	private static final String ADDRESS = "192.168.100.33";
+	private static final int PORT = 2222;
+	
+	public static void main(String[] args) {
+		try (Socket socket = new Socket("localhost", PORT);) {
+			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+			DataInputStream dis = new DataInputStream(socket.getInputStream());
+			Scanner scan = new Scanner(System.in);
+			
+			Thread writeTextThead = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					String read = null;
+					try {
+						while ((read = dis.readUTF()) != null) {
+							System.out.println(read);
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			writeTextThead.start();
+			
+			while (true) {
+				String write = null;
+				write = scan.nextLine();
+				if (write.equals("/종료")) {
+					break;
+				}
+				dos.writeUTF(write);
+				dos.flush();
+			}
+		} catch (UnknownHostException e) {
+			// 어케 해결함?????????????????????????!!#@!#!@#@!#!@
+//			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -6,16 +6,15 @@ import java.net.Socket;
 
 public class Server {
 	private final static int PORT = 2222;
-	private static UserChat userEnterace;
 	
 	public static void main(String[] args) {
 		try (ServerSocket server = new ServerSocket(PORT)){
+			System.out.println("서버 오픈");
 			while (true) {
-				System.out.println("서버 오픈");
 				Socket socket = server.accept();
 				
-				System.out.println("사용자 입장");
-				userEnterace = new UserChat(socket);
+				Chat userEnterace = new Chat(socket);
+				userEnterace.start();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
