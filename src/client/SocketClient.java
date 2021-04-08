69931caceba9b91b2d620807f6ff14e2a2072afe
skawp1 <8shot@naver.com>
2021-04-08 15:25:20 +0900

    Server 테스트

--- src/client/SocketClient.java
+++ src/client/SocketClient.java
@@ -13,11 +13,13 @@ import library.User;
 // GUI 에서 입력한 로그인, 회원가입 객체 -> 서버로 전송
 public class SocketClient {
 //	private final String SERVER_ADDRESS = "192.168.100.33";
-	private final String SERVER_ADDRESS = "localhost";
-	private final int PORT = 2222;
+	private final static String SERVER_ADDRESS = "localhost";
+	private final static int PORT = 2222;
+	
+	static LoginResult result = null;
+	static User user = new User();
 	
 	public LoginResult login(User user) {
-		LoginResult result = null;
 		try (Socket socket = new Socket(SERVER_ADDRESS,	PORT)) {
 			
 			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
@@ -25,6 +27,7 @@ public class SocketClient {
 			
 			oos.writeObject(user);	// 서버에 객체 전달
 			oos.flush();
+			
 			
 			result = (LoginResult) ois.readObject();
 			
@@ -53,7 +56,7 @@ public class SocketClient {
 	
 	public static void main(String[] args) {
 		SocketClient client = new SocketClient();
-		LoginResult result = client.login(new User("보라돌이", "1111"));
+		LoginResult result = client.login(new User("moderator1", "moderator1"));
 		System.out.println(result);
 	}
 }
