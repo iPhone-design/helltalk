a4b50ce7a77226b55c07932becefcf5d8bbcf652
user <user@DESKTOP-ENPUSVT>
2021-04-08 11:29:52 +0900

    User 객체 추가

---
+++ src/library/User.java
@@ -0,0 +1,121 @@
+package library;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 2222L;
+	
+	private String id;
+	private String password;
+	private String nickname;
+	private int age;
+	private int active;
+	
+	public User(String id, String password) {
+		super();
+		this.id = id;
+		this.password = password;
+	}
+	
+	public User(String id, String password, String nickname, int age) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.nickname = nickname;
+		this.age = age;
+	}
+	
+	public User() {
+		super();
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getActive() {
+		return active;
+	}
+
+	public void setActive(int active) {
+		this.active = active;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", password=" + password + ", nickname=" + nickname + ", age=" + age + ", active="
+				+ active + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + active;
+		result = prime * result + age;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nickname == null) ? 0 : nickname.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (active != other.active)
+			return false;
+		if (age != other.age)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nickname == null) {
+			if (other.nickname != null)
+				return false;
+		} else if (!nickname.equals(other.nickname))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+}
