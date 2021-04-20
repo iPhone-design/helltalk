ed16153ecbfa1140ebdd2ac36414501d7947c965
skawp1 <8shot@naver.com>
2021-04-20 10:50:36 +0900

    Client에 Localhost 추가

--- src/client/Client.java
+++ src/client/Client.java
@@ -10,12 +10,13 @@ import gui.MainFrame;
 
 
 public class Client {
-	private static final String ADDRESS = "192.168.100.33";
+//	private static final String ADDRESS = "192.168.0.71";
+	private static final String ADDRESS = "Localhost";
 	private static final int PORT = 2222;
 	
 	public static void main(String[] args) {
 		try {
-			Socket socket = new Socket("localhost", PORT);
+			Socket socket = new Socket(ADDRESS, PORT);
 			MainFrame main = new MainFrame(socket);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
